d38af693481461f12086e0656c2b52cf0314b2f6
Joao Dias <jvdiasrodrigues2006@gmail.com>
2025-05-01 22:58:30 -0300

    melhoria na extracao de dados + melhoria nos scripts do bot + base simples para testar o bot

--- src/documentos.py
+++ src/documentos.py
@@ -1,11 +1,46 @@
 from datetime import datetime
 import pandas as pd
+import google.generativeai as genai
 
 hoje = datetime.now().strftime("%d/%m/%Y")
 
-df_proximas = pd.read_csv("teste_proximas.csv")
-df_ultimas = pd.read_csv("teste_ultimas.csv")
-df_player_stats = pd.read_csv("teste_estats_jogador.csv")
+df_proximas = pd.read_json("base_proximas.json")
+df_ultimas = pd.read_json("base_ultimas.json")
+df_player_stats = pd.read_json("base_stats_jogador.json")
+
+
+modelo_doc = genai.GenerativeModel(
+    model_name = "gemini-2.0-flash-001",
+    system_instruction="""""""""
+        Você é o assistente oficial do time de e-sports FURIA. Sua principal função é conversar com os fãs do time — conhecidos como "furiosos" — e ajudá-los com informações sobre a FURIA.
+
+        Seu tom deve ser empolgado, amigável e sempre leal à identidade combativa e intensa da FURIA sempre. Você deve:
+
+        - Chamar o usuário de "furioso" ou "furiosa" sempre que se referir a ele/ela.
+        - Responder perguntas sobre resultados de partidas da FURIA (League of Legends(LTA Sul).).
+        - Fornecer informações sobre jogadores, escalações, torneios e próximos jogos.
+        - Compartilhar curiosidades, feitos históricos e novidades do time quando solicitado.
+        - Estimular o engajamento dos fãs, como convidar para assistir aos jogos ou seguir as redes da FURIA.
+        - Jamais inventar informações. Se algo não estiver confirmado, diga que "ainda não foi anunciado oficialmente pela FURIA" ou similar.
+        - A cada número aleatório de mensagens (entre 3 e 6 perguntas respondidas), envie sua resposta normalmente e inclua uma mensagem incentivando o furioso(a) a visitar a loja oficial da FURIA para conferir os produtos e roupas do time. Exemplo: “Aproveita e confere os novos drops insanos na loja oficial da FURIA, furioso! Tá animal!”
+        - Você tem acesso direto a um ou mais arquivos JSON com informações sobre partidas, jogadores ou torneios da FURIA. Use esses dados sempre que forem relevantes para responder às perguntas dos usuários. Nunca invente nada — se algo não estiver nos dados ou não for oficial, diga que não há informação confirmada.
+        - **E haja de uma forma engracada e satirica quando perguntarem a respeito de outros times**.
+
+        IMPORTANTE: 
+        - NAO EXISTEM OUTRO JOGADORES ALEM DO GUIGO,TATU,TUTZ,AYU E JOJO
+        - NAO RETORNE QUALQUER NOME DE JOGADOR QUE NAO SEJA DELES
+        - SE TEM UMA INFORMACAO QUE USARIO PEDIR NAO ESTIVER NOS DOCUMENTOS, DIGA QUEM NAO TEM INFORMACOES A RESPEITO DISSO
+        - E NUNCA INVENTE RESPOSTA.
+
+        Exemplo de tom:
+        - “Fala, furioso! Que jogaço ontem, hein? Quer saber os destaques da partida?”
+        - “Tamo junto, furiosa! A FURIA entra em jogo amanhã às 18h, horário de Brasília. É vencer ou vencer!”
+
+        Seja breve, direto e sempre envolvente. Sua missão é representar a FURIA com garra e manter os furiosos bem informados e animados!
+
+    """""""""
+)
+
 
 history_inicial = [
     {
@@ -14,22 +49,91 @@ history_inicial = [
     },
     {
         "role": "user",
-        "parts": [f"Usar o arquivo {df_proximas} para responder perguntas a respeito de próximas partidas da FURIA na LTA Sul ou CBLOL, e retorne a que tem a data mais próxima do dia de hoje."]
+        "parts": [f"""Usar o arquivo {df_proximas} para responder perguntas a respeito de próximas partidas da FURIA na LTA Sul ou CBLOL,  dentro de {df_proximas} retorne a que tem a data mais próxima do dia de hoje.
+                   PERGUNTAS QUE PODEM VIR: 
+                   - Quando vai ser a proxima partida da Furia? (ou semelhante)
+                  
+                   IMPORTANTE:
+                   NUNCA USE O DF{df_ultimas} PARA RESPONDER A PERGUNTAS SOBRE PROXIMAS PARTIDAS"""]
     },
     {
         "role": "user",
-        "parts": [f"Usar o arquivo {df_ultimas} para responder perguntas a respeito de últimas partidas da FURIA na LTA Sul ou CBLOL, e retorne a que tem a data mais próxima do dia de hoje."]
+        "parts": [f"""Usar o arquivo {df_ultimas} para responder perguntas a respeito de últimas partidas da FURIA na LTA Sul ou CBLOL, e dentro de {df_ultimas} retorne a que tem a data mais próxima do dia de hoje.
+                   PERGUNTAS QUE PODEM VIR: 
+                   - Quando foi a ultima partida da Furia? (ou semelhante)
+
+                   IMPORTANTE:
+                   NUNCA USE O DF{df_proximas} PARA RESPONDER A PERGUNTAS SOBRE ULTIMAS PARTIDAS"""]
     },
     {
         "role": "user",
-        "parts": [f"""Usar o arquivo {df_player_stats} para responder perguntas a respeito das estatisticas individuais ou a perguntas de como foi a partida como kills,mortes,assistencias,campeao,etc de cada jogador da FURIA seja por rota ou nome na LTA Sul ou CBLOL,
-                   use tambem o {df_ultimas}, compare as datas dos dois arquivos para responder com precisao a partida caso facam perguntas como exemplo: Qual foi a estatistica de um certo jogador na
-                   ultima partida. Use o {df_player_stats} para ver se foi uma partida com mais de 1 partida e retorne falando a respeito de ter sido ou uma melhor de tres , ou uma melhor de cinco
-                   e caso for perguntado tambem a respeito de estatisticas individuais dos jogadores retorne as estatitisca das partidas que tiveram no dia. 
-                   Sempre antes de responder, se a FURIA ter vencido, diga algo engracado tipo: Esse jogo foi um amasso!!!, Esse jogo foi um passeio!!!,etc. E some as estatisca da partida do time
-                   da FURIA em abates e  mortes e compare com o time inimigo.
-                   Se voce for perguntado a respeito de como foi a partida retorne de uma forma mais descontraida sobre os resultados e uma forma diver Seu tom deve ser empolgado, amigável e sempre leal
-                   à identidade combativa e intensa da FURIA sempre. E use o {df_player_stats} pegue o numeros de kills e mortes de uma partida de cada vez.
+        "parts": [f"""
+                    Sempre que perguntarem sobre a organizacao da FURIA ou algo do tipo responda com seguintes informacoes:Somos FURIA.
+                    Uma organização de esports que nasceu do desejo de representar o Brasil no CS e conquistou muito mais que isso: expandimos nossas ligas, disputamos os principais títulos, adotamos novos objetivos e ganhamos um propósito maior. Somos muito mais que o sucesso competitivo.
+                    Somos um movimento sociocultural.
+                    Nossa história é de pioneirismo, grandes conquistas e tradição. Nosso presente é de desejo, garra e estratégia. A pantera estampada no
+                    peito estampa também nosso futuro de glória. Nossos pilares de performance, lifestyle, conteúdo, business, tecnologia e social são os principais constituintes do movimento FURIA, que representa uma unidade que respeita as individualidades e impacta positivamente os contextos em que se insere. Unimos pessoas e alimentamos sonhos dentro e fora dos jogos.
+                    Nossa história é de pioneirismo, grandes conquistas e tradição. Nosso presente é de desejo, garra e estratégia. A pantera estampada no
+                    peito estampa também nosso futuro de glória. Nossos pilares de performance, lifestyle, conteúdo, business, tecnologia e social são os principais constituintes do movimento FURIA, que representa uma unidade que respeita as individualidades e impacta positivamente os contextos em que se insere. Unimos pessoas e alimentamos sonhos dentro e fora dos jogos.
+                    """]
+    },
+    {
+        "role": "user",
+        "parts": [f"""
+                    Sempre que perguntarem a respeito de titulos da FURIA responda com as seguintes informacoes:
+                    O time de CS:GO da FURIA é um dos mais notáveis, com destaque para:
+
+                    IEM Rio Major 2022: Primeira equipe brasileira classificada para o torneio .
+                    The Enemy
+
+                    PGL Major Stockholm 2021: Chegada às quartas de final, consolidando-se como uma das principais equipes da região .
+
+                    PGL Major Antwerp 2022: Novo recorde com a classificação para as quartas de final .
+
+                    DreamHack Masters Spring 2020: Vitória na região da América do Norte .
+                    Mais Esports+3ge+3The Enemy+3
+
+                    ESL Pro League Season 12: North America: Conquista significativa na temporada .
+                    Liquipedia+6dust2.com.br+6Red Bull+6
+
+                    Elisa Invitational Summer 2021: Título importante na competição .
+
+                    Em 2023, a FURIA também se destacou ao vencer os dois campeonatos de CS:GO na Brasil Game Show (BGS), tanto no masculino quanto no feminino 
+                    """]
+    },
+    {
+        "role": "user",
+        "parts": [f"""Usar o arquivo {df_player_stats} para responder perguntas a respeito das estatisticas individuais ou a perguntas de como foi a partida ultima partida.
+                   **Nunca** retorne estatisticas somadas da FURIA
+                   Antes de retornar qualquer coisa veja se teve mais do oque uma partida da FURIA e apenas da FURIA no mesmo dia, no caso uma MD3 ou MD5, use a 'DATA' dentro de {df_player_stats}
+                   Dai depois de verificar se teve mais de uma partida em {df_player_stats} no mesmo dia, voce retorna as seguinte informacoes da FURIA de todas as partidas que tiveram no dia, informacoes de qual foi primeira partida e estatistica de cada jogador(colete as informacoes que estao na mesma fileira):
+                   'PARTIDA' 'RESULTADO DA PARTIDA'
+                   'NOME JOGADOR' 'ROTA' 'KILLS' 'MORTES''ASSISTENCIAS' 'CAMPEAO'
+                   retorne de uma forma mais descontraida sobre os resultados e uma forma diver Seu tom deve ser empolgado, amigável e sempre leal à identidade combativa e intensa da FURIA sempre.
+                   **Nunca** retorne estatisticas somadas da FURIA
+                   Sempre antes de responder, se a FURIA ter vencido, diga algo engracado tipo: Esse jogo foi um amasso!!!, Esse jogo foi um passeio!!!,etc.
+                   E retorne tambem que foi o adversario da FURIA pegue a 'DATA' do {df_player_stats} e compara com a 'DATA' do {df_ultimas}, as partidas que tiverem os dias iguais retorne a partida.
+                   **EXEMPLO**:
+
+                   'df_player_stats'
+                   
+                   "DATA": "2025-04-27T17:02:02.000+00:00",
+                   
+                   'df_ultimas'
+                  
+                   "DATA":"27\/04\/2025 14:05",
+                 
+                   **RETORNE (COM LINGUAGEM NATURAL)**
+
+                   'df_ultimas'
+                   
+                   "TIME 1":"FURIA Esports",
+                   "PLACAR":"2 - 1",
+                   "TIME 2":"LOUD",
+                     
+
+                   Caso contrario retorne que ainda nao tem informacoes oficiais 
+
                   """""]
     }
 ]
--- src/extracao_de_dados/estatisticas_jogadores.py
+++ src/extracao_de_dados/estatisticas_jogadores.py
@@ -1,7 +1,11 @@
 import requests
 import pandas as pd
+import json
+import os
 
-url = "https://api.bo3.gg/api/v1/lol/stats/matches/85448/players_stats"
+arquivo = "base_stats_jogador.json"
+
+url = "https://api.bo3.gg/api/v1/lol/stats/matches/85457/players_stats"
 
 response = requests.get(url)
 
@@ -15,12 +19,18 @@ if response.status_code == 200:
     for match_stats in data:
         dia = match_stats['begin_at']
         numero_partida = match_stats['game_number']
+        vitoria_codigo = match_stats['winner_team_id']
+        if (vitoria_codigo == 17448):
+            resultado = "FURIA VENCEU"
+        else:
+            resultado = "FURIA PERDEU"    
 
         for player in match_stats['lol_game_players']:
-            rota = player.get('role')
             time = player.get('team_id')
             if(time == 	17448):
                 time = "FURIA"
+                rota = player.get('role')
+                
                 match rota:
                     case "top":
                         nome_jogador = "Guigo"
@@ -31,26 +41,34 @@ if response.status_code == 200:
                     case "adc":
                         nome_jogador = "Ayu"
                     case "sup":
-                        nome_jogador = "Jojo"           
-            else:
-                nome_jogador = "Outro time"
-                time = "OUTRO TIME"
-            kills = player.get('kills') 
-            mortes = player.get('deaths')
-            assistencias = player.get('assists') 
-            campeao = player['lol_champion']['name']
+                        nome_jogador = "Jojo"   
 
-            all_data.append({"DATA":dia,
-                             "PARTIDA": numero_partida,
-                             "TIME":time,
-                             "NOME JOGADOR": nome_jogador ,
-                             "ROTA":rota,
-                             "CAMPEAO":campeao,
-                             "KILLS":kills,
-                             "MORTES":mortes,
-                             "ASSISTENCIAS":assistencias
-                             })
+                time = player.get('team_id')                        
+                kills = player.get('kills') 
+                mortes = player.get('deaths')
+                assistencias = player.get('assists') 
+                campeao = player['lol_champion']['name']
+
+                all_data.append({"DATA":dia,
+                                "PARTIDA": numero_partida,
+                                "RESULTADO DA PARTIDA": resultado,
+                                "TIME":time,
+                                "NOME JOGADOR": nome_jogador ,
+                                "ROTA":rota,
+                                "CAMPEAO":campeao,
+                                "KILLS":kills,
+                                "MORTES":mortes,
+                                "ASSISTENCIAS":assistencias
+                                })
 
     df_stats_jogadores = pd.DataFrame(all_data)
 
-df_stats_jogadores.to_csv("teste_estats_jogador.csv", index=False, mode="a")
+if not os.path.exists(arquivo):
+    # Se o arquivo não existir, cria com o DataFrame diretamente
+    df_stats_jogadores.to_json(arquivo, orient="records", indent=4, force_ascii=False, index=False)
+else:
+    with open("base_stats_jogador.json", "r+", encoding="utf-8") as f:
+        dados = json.load(f)
+        dados.extend(df_stats_jogadores.to_dict(orient="records"))
+        f.seek(0)
+        json.dump(dados, f, indent=4, ensure_ascii=False)
